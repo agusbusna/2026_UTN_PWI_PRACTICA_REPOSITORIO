2d7b2850f1114015c052c8b26709b3d90a122e1c
agusbusna <agustinbusna@gmail.com>
2026-09-02 12:07:48 -0300

    "Clase 23"

--- Clase 21/src/App.jsx
+++ Clase 21/src/App.jsx
@@ -1,69 +1,153 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './global.css'
-import ProductCard from './Componentes/ProductCard/ProductCard'
-import Counter from "./Componentes/Counter/Counter.jsx"
+import ContactList from './Componentes/ContactsList/contactsList'
+
+
 
 export default function App() {
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState(null)
+    const [isLoading, setIsloading] = useState(true)
 
+    const resultado = fetch (
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+            method: "GET",
+        }
+    )
     /* 
-    useState es una funcion de react que te permite declarar estados
-    useState recibe el valor incial de tu estado
-    useState retorna un array de 2 posiciones (tupla)
-        La primera posicion representa el valor del estado en si
-        La segunda posicion del array representa la funcion de setteo (la funcion para actualizar el estado)
-
-    LOS ESTADOS SON INMUTABLES, si queremos modificar/actualizar el valor de un estado debemos hacerlo mediante el setter
+    JS maneja la asincronia mediante promesas (es un objeto especial)
+    Una promesa tiene tres estados posibles:
+        Pendiente - Mientras la operacion asincronica no este resuelta
+            Resuelta - Cuando la promesa se resolvio efectivamente
+            Rechazada - Si la promesa no se pudo resolver (hubo un error)
+     */
+    /* 
+    fetch es una funcion que ordena
+        es una funcion asincronica  y me va a devolver el resultado de la consulta 
     */
-    /*  const modalAbiertoEstado = useState(false)
-    const modalAbierto = modalAbiertoEstado[0]
-    const setModalAbierto = modalAbiertoEstado[1]  */
-    const [modalAbierto, setModalAbierto] = useState(false)
+    console.log ("Inmediatamente muestra el resultado", resultado)
 
+    async function consultarAlServidor (){
+        const respuesta_http = await fetch (
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+            method: "GET",
+        }
+    )
+    console.log ("Aguarde a que la tarea sea resuelva y muestro el resultado ", respuesta_http)
+    const resultado = await respuesta_http.json()
+    console.log (resultado)
+    setIsloading (false)
+    setResponse (resultado)
+    }
+    useEffect(
+        () => {
+            consultarAlServidor()
+        },
+        []
+    )
+    console.log (isLoading, response)
 
-    function abrirModal() {
-        setModalAbierto(true)
+    if(isLoading){
+        return (
+            <div>Cargando</div>
+        )
     }
 
-    console.log('[App.jsx] Me renderize')
+    const lista_posteos_jsx = []
+    for (const post of response){
+        lista_posteos_jsx.push(
+            <div key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <hr/>
+            </div>
+        )
+    }
+    
     return (
         <div>
-            <button onClick={abrirModal}>Abrir modal</button>
-            {
-                modalAbierto
-                &&
-                <div className='modal-container'>
-                    <div className='modal'>
-                        <h1>Hola soy un modal</h1>
-                        <button>Cerrar</button>
-                    </div>
-                </div>
-            }
-            <Counter/>
-
+            {lista_posteos_jsx}
         </div>
     )
 }
-
-
-
-const persona_1 = ['pepe', 'suarez', 50]
-
-const [nombre, apellido, edad] = persona_1
-/* const persona_nombre = persona_1[0]
-const persona_apellido = persona_1[1]
-const pesona_edad = persona_1[2] */
-
-/* 
-Counter.jsx
-Desarrollar el componente Counter que sera un contador
-El contador debera contar con un boton para incrementar, otro para decrementar y el visualizador del contador
-El contador debe funcionar, es decir debe poder aumentar y decrementar al presionar los botones.
-El contador debe iniciar en 0
+/*  
+Contactos:
+[
+{
+    id: 1
+    nombre: 'Marcos',
+    ultimo_mensaje: 'hay que juntarnos!..',
+    imagen: "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
+    mensajes_sin_leer: 2,
+    fecha_ultimo_mensaje: "12/2/2023 14:30"
+},
+...sigan completando con almenos 3 contactos
+]
+Renderizar la lista de contactos
+Ejemplo: https://user-images.githubusercontent.com/44744039/224526440-b4eb6a54-4dda-430c-a681-286f5b406fe7.png
 */
+
+
 /* 
-import Counter from './Componentes/Counter/Counter';
+const mensajes_servidor = [
+        {
+            id: 1,
+            autor: "jose",
+            contenido: "Hola!",
+            fecha: "26 de agosto 2026 11:45"
+        },
+        {
+            id: 2,
+            autor: "maria",
+            contenido: "Todo bien?",
+            fecha: "26 de agosto 2026 11:50"
+        },
+        {
+            id: 3,
+            autor: "juan",
+            contenido: "Hola! Todo bien, ustedes?",
+            fecha: "26 de agosto 2026 11:45"
+        },
+        {
+            id: 4,
+            autor: "julia",
+            contenido: "Hola!",
+            fecha: "26 de agosto 2026 11:45"
+        },
+        {
+            id: 5,
+            autor: "diego",
+            contenido: "Hola!",
+            fecha: "26 de agosto 2026 11:45"
+        }
+    ]
+/* 
+    En react tenemos listas de JSX
+    Las listas de JSX son arrays donde cada elemento tiene una expresion JSX
+    Las listas JSX se renderizan directamente en la pantalla de forma ordenada
+     */
+/*  const mensajes_jsx = []
+    for (const mensaje of mensajes_servidor) {
+        mensajes_jsx.push(
+            <div key={mensaje.id}>
+                <h2>{mensaje.autor}</h2>
+                <p>{mensaje.contenido}</p>
+                <span>Fecha: {mensaje.fecha}</span>
+                <hr />
+            </div>
+        )
+    }
 
-function App() {
-    return <Counter />;
-} */
 
+    return (
+        <div>
+            {mensajes_jsx}
+            <ContactList/>
+        </div>
+    )
+    }
+    
+    
+ */
