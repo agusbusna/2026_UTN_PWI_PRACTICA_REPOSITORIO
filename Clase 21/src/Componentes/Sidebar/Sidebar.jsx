9b710a530d1f2974a54232aec1a1f8da0aeaa8ae
agusbusna <agustinbusna@gmail.com>
2026-09-13 14:28:38 -0300

    tarea-final-contactos-sidebar

--- Clase 21/src/Componentes/Sidebar/Sidebar.jsx
+++ Clase 21/src/Componentes/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar() {
                 {
                     id: 1,
                     nombre: 'Marcos',
-                    ultimo_mensaje: 'hay que juntarnos!..',
+                    ultimo_mensaje: 'Hey qué tal? Hay que juntarnos! La semana que viene es mi cumple y estas invitada',
                     imagen: "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
                     mensajes_sin_leer: 2,
                     fecha_ultimo_mensaje: "14:30"
@@ -16,7 +16,7 @@ export default function Sidebar() {
                 {
                     id: 2,
                     nombre: "John Smith",
-                    ultimo_mensaje: "Testing",
+                    ultimo_mensaje: "Testeando... 1, 2, 3, probando",
                     imagen: "https://i.pinimg.com/736x/89/28/4b/89284b5c67cac49c7b0be588a335932a.jpg",
                     mensajes_sin_leer: 3,
                     fecha_ultimo_mensaje: "08:21"
@@ -24,7 +24,7 @@ export default function Sidebar() {
                 {
                     id: 3,
                     nombre: "Jane Doe",
-                    ultimo_mensaje: "Hello there",
+                    ultimo_mensaje: "Holaaaaaaa, tanto tiempo",
                     imagen: "https://i.pinimg.com/736x/1a/98/03/1a9803d93ff615ea7fbc198ba53401c5.jpg",
                     mensajes_sin_leer: 1,
                     fecha_ultimo_mensaje: "12:15"
@@ -32,7 +32,7 @@ export default function Sidebar() {
                 {
                     id: 4,
                     nombre: "Samantha Lee",
-                    ultimo_mensaje: "See you tumorrow",
+                    ultimo_mensaje: "Genial, Marcos, estoy!",
                     imagen: "https://i.pinimg.com/736x/b7/5f/d2/b75fd2fc1f8cbb7f65bb4f1a87dcc71e.jpg",
                     mensajes_sin_leer: null,
                     fecha_ultimo_mensaje: "09:35"
