b31507ffbc7f511261c77c97f45ba83d5b2cdc2e
agustin.busna <agustinbusna@gmail.com>
2026-09-14 09:51:57 -0300

    "correcion"

--- Clase 21/src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
+++ Clase 21/src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router'
 import "./ContactDetailScreen.css"
+import Sidebar from '../../Componentes/Sidebar/Sidebar'
 
 export default function ContactDetailScreen() {
 
@@ -9,7 +10,7 @@ export default function ContactDetailScreen() {
         {
             id: 1,
             nombre: 'Marcos',
-            ultimo_mensaje: 'hay que juntarnos!..',
+            ultimo_mensaje: 'Hey qué tal? Hay que juntarnos! La semana que viene es mi cumple y estas invitada',
             imagen: "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
             mensajes_sin_leer: 2,
             fecha_ultimo_mensaje: "14:30"
@@ -17,7 +18,7 @@ export default function ContactDetailScreen() {
         {
             id: 2,
             nombre: "John Smith",
-            ultimo_mensaje: "Testing",
+            ultimo_mensaje: "Testeando... 1, 2, 3, probando si funciona esto",
             imagen: "https://i.pinimg.com/736x/89/28/4b/89284b5c67cac49c7b0be588a335932a.jpg",
             mensajes_sin_leer: 3,
             fecha_ultimo_mensaje: "08:21"
@@ -25,7 +26,7 @@ export default function ContactDetailScreen() {
         {
             id: 3,
             nombre: "Jane Doe",
-            ultimo_mensaje: "Hello there",
+            ultimo_mensaje: "Holaaaaaaa queridoooo tanto tiempo",
             imagen: "https://i.pinimg.com/736x/1a/98/03/1a9803d93ff615ea7fbc198ba53401c5.jpg",
             mensajes_sin_leer: 1,
             fecha_ultimo_mensaje: "12:15"
@@ -33,7 +34,7 @@ export default function ContactDetailScreen() {
         {
             id: 4,
             nombre: "Samantha Lee",
-            ultimo_mensaje: "See you tumorrow",
+            ultimo_mensaje: "Genial, vamos juntos!",
             imagen: "https://i.pinimg.com/736x/b7/5f/d2/b75fd2fc1f8cbb7f65bb4f1a87dcc71e.jpg",
             mensajes_sin_leer: null,
             fecha_ultimo_mensaje: "09:35"
@@ -56,18 +57,21 @@ export default function ContactDetailScreen() {
         )
     }
     return (
+
+        
         <div className='body-screen-detail'>
+            <Sidebar/>
             <div className='contact-detail-screen'>
                 <Link to="/home" className="btn-volver">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M19 12H5M12 19l-7-7 7-7" />
+                        <path d="M19 12H5M12 19l-7-7 7-7" />
                     </svg>
                 </Link>
                 <div className='img-contact-detail-container'>
                     <img src={contacto_seleccionado.imagen} alt={contacto_seleccionado.nombre} className='img-contact-detail' />
                 </div>
                 <h1 className='nombre-contacto-detail'>{contacto_seleccionado.nombre}</h1>
-                
+
             </div >
         </div>
     )
